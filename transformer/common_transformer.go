276893b4cc5c5bb5de8de8961ec3ff56701b3106
andrewesteves <esteves.andrew@gmail.com>
2019-12-01 20:29:32 -0300

    Main branch clear

--- transformer/common_transformer.go
+++
@@ -1,9 +0,0 @@
-package transformer
-
-// CommonTransformer struct
-type CommonTransformer struct {
-	Current string `json:"current"`
-	Prev    string `json:"prev"`
-	Next    string `json:"next"`
-	Total   string `json:"total"`
-}
