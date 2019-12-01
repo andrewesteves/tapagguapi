e1dff59687ab7f3222db7f4c147647387bb6d66d
andrewesteves <esteves.andrew@gmail.com>
2019-12-01 20:22:54 -0300

    Cleanup for main branch

--- go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/andrewesteves/tapagguapi
-
-go 1.13
-
-require (
-	github.com/gorilla/mux v1.7.3
-	github.com/lib/pq v1.2.0
-)
--- model/company.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type Company struct {
-	ID    int64  `json:"id"`
-	CNPJ  string `json:"cnpj" xml:"CNPJ"`
-	Name  string `json:"name" xml:"xNome"`
-	Title string `json:"title" xml:"xFant"`
-}
