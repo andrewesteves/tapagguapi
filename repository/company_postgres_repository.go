822792c0f15cf73872719a674bee42ec02a3bb2b
andrewesteves <esteves.andrew@gmail.com>
2019-12-11 20:35:08 -0300

    Company with address info

--- repository/company_postgres_repository.go
+++ repository/company_postgres_repository.go
@@ -21,14 +21,14 @@ func NewCompanyPostgresRepository(Conn *sql.DB) CompanyContractRepository {
 // All companies
 func (r CompanyPostgresRepository) All(user model.User) ([]model.Company, error) {
 	var companies []model.Company
-	rs, err := r.Conn.Query("SELECT id, cnpj, name, title FROM companies WHERE user_id = $1", user.ID)
+	rs, err := r.Conn.Query("SELECT id, cnpj, name, title, street, number, district, city, state, zipcode FROM companies WHERE user_id = $1", user.ID)
 	if err != nil {
 		return nil, err
 	}
 
 	for rs.Next() {
 		var company model.Company
-		err = rs.Scan(&company.ID, &company.CNPJ, &company.Name, &company.Title)
+		err = rs.Scan(&company.ID, &company.CNPJ, &company.Name, &company.Title, &company.Street, &company.Number, &company.District, &company.City, &company.State, &company.Zipcode)
 		if err != nil {
 			return nil, err
 		}
@@ -41,7 +41,7 @@ func (r CompanyPostgresRepository) All(user model.User) ([]model.Company, error)
 // Find company
 func (r CompanyPostgresRepository) Find(id int64) (model.Company, error) {
 	var company model.Company
-	err := r.Conn.QueryRow("SELECT id, cnpj, name, title FROM companies WHERE id = $1", id).Scan(&company.ID, &company.CNPJ, &company.Name, &company.Title)
+	err := r.Conn.QueryRow("SELECT id, cnpj, name, title, street, number, district, city, state, zipcode FROM companies WHERE id = $1", id).Scan(&company.ID, &company.CNPJ, &company.Name, &company.Title, &company.Street, &company.Number, &company.District, &company.City, &company.State, &company.Zipcode)
 	if err != nil {
 		return model.Company{}, err
 	}
@@ -51,7 +51,7 @@ func (r CompanyPostgresRepository) Find(id int64) (model.Company, error) {
 // Store company
 func (r CompanyPostgresRepository) Store(company model.Company) (model.Company, error) {
 	lastInsertID := 0
-	err := r.Conn.QueryRow("INSERT INTO companies (user_id, cnpj, name, title, created_at, updated_at) VALUES ($1,$2,$3,$4,now(),now()) RETURNING id", company.User.ID, company.CNPJ, company.Name, company.Title).Scan(&lastInsertID)
+	err := r.Conn.QueryRow("INSERT INTO companies (user_id, cnpj, name, title, street, number, district, city, state, zipcode, created_at, updated_at) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,now(),now()) RETURNING id", company.User.ID, company.CNPJ, company.Name, company.Title, company.Street, company.Number, company.District, company.City, company.State, company.Zipcode).Scan(&lastInsertID)
 	if err != nil {
 		return model.Company{}, err
 	}
@@ -68,11 +68,11 @@ func (r CompanyPostgresRepository) Update(company model.Company) (model.Company,
 	if u.ID < 1 {
 		return model.Company{}, errors.New("Cant't find this company id")
 	}
-	rs, err := r.Conn.Prepare("UPDATE companies SET cnpj = $1, name = $2, title = $3, updated_at = now() WHERE id = $4")
+	rs, err := r.Conn.Prepare("UPDATE companies SET cnpj = $1, name = $2, title = $3, street = $4, number = $5, district = $6, city = $7, state = $8, zipcode = $9, updated_at = now() WHERE id = $10")
 	if err != nil {
 		return model.Company{}, err
 	}
-	rs.Exec(company.CNPJ, company.Name, company.Title, company.ID)
+	rs.Exec(company.CNPJ, company.Name, company.Title, company.Street, company.Number, company.District, company.City, company.State, company.Zipcode, company.ID)
 	return company, nil
 }
 
@@ -95,7 +95,7 @@ func (r CompanyPostgresRepository) Destroy(id int64) (model.Company, error) {
 
 // FindBy company
 func (r CompanyPostgresRepository) FindBy(company model.Company, field string, value interface{}) (model.Company, error) {
-	err := r.Conn.QueryRow(fmt.Sprintf("SELECT id, cnpj, name, title FROM companies WHERE %s = $1 AND user_id = $2", field), value, company.User.ID).Scan(&company.ID, &company.CNPJ, &company.Name, &company.Title)
+	err := r.Conn.QueryRow(fmt.Sprintf("SELECT id, cnpj, name, title, street, number, district, city, state, zipcode FROM companies WHERE %s = $1 AND user_id = $2", field), value, company.User.ID).Scan(&company.ID, &company.CNPJ, &company.Name, &company.Title, &company.Street, &company.Number, &company.District, &company.City, &company.State, &company.Zipcode)
 	if err != nil {
 		return model.Company{}, err
 	}
